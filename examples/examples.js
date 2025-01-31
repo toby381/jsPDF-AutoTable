56e930a20d039672ab095f8f6f4d7c67adf68ea0
Torbjørn Høvde <torbjorn.hovde@norrona.no>
2025-01-31 10:28:08 +0100

    imagegrid 2

--- examples/examples.js
+++ examples/examples.js
@@ -1,44 +1,150 @@
 var examples = {}
 window.examples = examples
+var totalPagesExp = 'totalpagescountstringtobereplaced'
 
-examples.techpack = function () {
+examples.techPack = function () {
   var doc = new jsPDF({
     orientation: 'l',
     unit: 'mm',
     format: 'a4',
     putOnlyUsedFonts: true,
   })
-  doc.addFileToVFS('DINLight-normal.ttf', light)
-  doc.addFont('DINLight-normal.ttf', 'DINLight', 'normal')
-  doc.addFileToVFS('DINNormal-normal.ttf', normal)
-  doc.addFont('DINNormal-normal.ttf', 'DINNormal', 'normal')
-  doc.addFileToVFS('DINBold-normal.ttf', bold)
-  doc.addFont('DINBold-normal.ttf', 'DINBold', 'normal')
-  doc.addFileToVFS('dinsmallcapspdf-normal.ttf', smallcaps)
-  doc.addFont('dinsmallcapspdf-normal.ttf', 'dinsmallcapspdf', 'normal')
 
-  var totalPagesExp = 'totalpagescountstringtobereplaced'
+  addFontPack(doc);
+  doc.setFontSize(7)
 
-  // image: 'image',
-  // supplier: 'supplier',
-  // material: 'material name',
-  // paidby: 'paid by',
-  // size: 'size',
-  // wheretouse: 'where to use',
-  // materialtype: 'material type',
-  // consume: 'consume',
-  // waste: 'waste',
-  // shrink: 'shrink.',
-  // csp3cons: 'csp3 cons.',
-  // patternrotation: 'pattern rotation',
-  // garmentrotation: 'garment rotation',
-  // grading: 'grading',
+
+  doc.autoTable({
+    theme: 'plain',
+    body: [
+      ['', '1. Brim mesh\n2. brim wire\n3. 3 edgle tape 20 mm\nHow to:'],
+    ],
+    columns: [
+      { header: ' ', dataKey: 'illustration' },
+      { header: ' ', dataKey: 'text' },
+    ],
+    columnStyles: { 
+      illustration: { cellPadding:5, minCellWidth: 120, minCellHeight: 170},
+      text: {cellPadding:5 } 
+
+    }, 
+    didDrawCell: async (data) => {
+      if (data.section === 'body' && data.row.index === 0 && data.column.index === 0) {
+        doc.addImage(imgsrc.base64, 'JPEG', data.cell.x , data.cell.y + 5 );      
+      }
+      if(data.section === 'body' && data.row.index === 0 && data.column.index === 1){
+        cellBorderLeft(doc, data, 0)
+      }
+    },
+    willDrawPage: function (data) {
+      addPageHeader(doc, data)
+    },
+    didDrawPage: function () {
+      addPageNumber(doc)
+    },
+  })
+
+  doc.addPage()
+
+  doc.autoTable({
+    theme: 'plain',
+
+    body:[
+      {
+        illustration: {
+          content: 'illu1',
+          colSpan: 0,
+          styles:{
+            minCellHeight: '110'
+          }
+        },
+        text: {
+          content: '1. Brim mesh\n2. brim wire\n3. 3 edgle tape 20 mm\nHow to:',
+          colSpan: 0,
+          styles:{
+            minCellHeight: '110'
+          }
+        },
+      },
+      {
+        illustration: {
+          content: 'illu2',
+          colSpan: 0,
+          styles:{
+            minCellHeight: '60'
+          }
+        },
+        text: {
+          content: '1. Brim mesh\n2. brim wire\n3. 3 edgle tape 20 mm\nHow to:',
+          colSpan: 0,
+          styles:{
+            minCellHeight: '60'
+          }
+        },
+      },
+    ],
+   
+    columns: [
+      { header: ' ', dataKey: 'illustration' },
+      { header: ' ', dataKey: 'text' },
+    ],
+    columnStyles: { 
+      illustration: { cellPadding:5, minCellWidth: 120},
+      text: {cellPadding:5 } 
+
+    }, 
+    didDrawCell: async (data) => {
+      if (data.section === 'body' && data.row.index === 0 && data.column.index === 0) {
+        doc.addImage(imgsrc.base64, 'JPEG', data.cell.x , data.cell.y + 10, 0, 90);      
+      }
+      if(data.section === 'body'  && data.column.index === 1){
+        cellBorderLeft(doc, data, 0)
+      }
+      if(data.section === 'body' && data.row.index === 1 && data.column.index === 0){
+        cellBorderTop(doc, data, 200, 5)
+        const xstart = data.cell.x
+        const ystart = data.cell.y + 10
+        const height = 60;
+
+        doc.addImage(imgsrc.base64, 'JPEG', xstart , ystart, 0 , height);      
+      }
+
+    },
+    willDrawPage: function (data) {
+      addPageHeader(doc, data)
+    },
+    didDrawPage: function () {
+      addPageNumber(doc)
+    },
+  })
+
+
+
+
+
+
+  doc.setFont('dinsmallcapspdf')
+  doc.putTotalPages(totalPagesExp)
+  return doc;
+};
+
+
+examples.BOMTable = function () {
+  var doc = new jsPDF({
+    orientation: 'l',
+    unit: 'mm',
+    format: 'a4',
+    putOnlyUsedFonts: true,
+  })
+  addFontPack(doc);
+  doc.setFontSize(7)
+
 
   let numberOfTableBreaks = 0;
   doc.autoTable({
     theme: 'plain',
-    startY: 15,
-    margin: 0,
+    margin: { top: 15 },
+    rowPageBreak: 'avoid',
     columns: [
       { dataKey: 'image' },
       { dataKey: 'supplier' },
@@ -57,6 +163,7 @@ examples.techpack = function () {
     ],
     styles: {
       lineWidth: 0,
+      fontSize: 7
     },
     headStyles: {
       font: 'DINNormal',
@@ -75,67 +182,25 @@ examples.techpack = function () {
     },
     didDrawCell: (data) => {
       if ([5, 7, 11, 13].includes(data.column.index)) {
-        doc.setDrawColor(0)
-        doc.line(
-          data.cell.x,
-          data.cell.y,
-          data.cell.x,
-          data.cell.y + data.cell.height,
-        )
-      } else if (
-        data.column.index > 0 &&
-        (data.section === 'body' ||
-          (data.section === 'head' && data.row.index === 2))
-      ) {
-        doc.setDrawColor(200)
-        doc.line(
-          data.cell.x,
-          data.cell.y,
-          data.cell.x,
-          data.cell.y + data.cell.height,
-        )
+        cellBorderLeft(doc, data, 0)
+      } else if (data.column.index > 0 && (data.section === 'body' || (data.section === 'head' && data.row.index === 2))) {
+        cellBorderLeft(doc, data, 200)
       }
       if (data.section === 'head' && data.row.index === 2) {
-        doc.setDrawColor(0)
-        doc.line(
-          data.cell.x,
-          data.cell.y,
-          data.cell.x + data.cell.width,
-          data.cell.y,
-        )
+       cellBorderTop(doc, data, 0)
+      }
+      if (data.section === 'body') {
+        cellBorderTop(doc, data, 200)
       }
     },
     willDrawPage: function (data) {
       numberOfTableBreaks +=1;
       // Header
-      doc.setFont('dinsmallcapspdf')
-      doc.setFontSize(10)
-      doc.setTextColor(40)
-      if (LOGO_SMALL) {
-        doc.addImage(LOGO_SMALL, 'JPEG', data.settings.margin.left, 5, 18, 5)
-      }
-
-      doc.text('SS2425', data.settings.margin.left + 23, 9)
-      doc.text('100120', data.settings.margin.left + 53, 9)
-      doc.text(
-        'lofoten Goretex pro plus Jacket Ms',
-        data.settings.margin.left + 78,
-        9,
-      )
-      doc.text('031220', data.settings.margin.left + 200, 9)
-      doc.setDrawColor(40)
-      doc.line(data.settings.margin.left, 13, 281, 13)
+      addPageHeader(doc, data)
     },
-    didDrawPage: function (data) {
-
-      // Footer
-      var str ='Page ' + doc.internal.getNumberOfPages() + ' of ' + totalPagesExp
-      doc.setFont('dinsmallcapspdf')
-      doc.setFontSize(10)
-      doc.setTextColor(40)
-      doc.text(str, 260, 9, { align: 'left' })
+    didDrawPage: function () {
+      addPageNumber(doc)
     },
-    margin: { top: 30 },
   })
 
   // table count output
@@ -146,7 +211,6 @@ examples.techpack = function () {
       doc.internal.pages[n][i] = doc.internal.pages[n][i].replace(replaceExpression, doc.pdfEscape16(`${n} / ${numberOfTableBreaks}`, doc.internal.getFont()));
     }
   }
-
   
   doc.setFont('dinsmallcapspdf')
   doc.putTotalPages(totalPagesExp)
@@ -154,11 +218,43 @@ examples.techpack = function () {
   return doc
 }
 
+
+
+
+
 /*
  |--------------------------------------------------------------------------
  | Below is some helper functions for the examples
  |--------------------------------------------------------------------------
  */
+
+function addPageNumber(doc) {
+  var str ='Page ' + doc.internal.getNumberOfPages() + ' of ' + totalPagesExp
+  doc.setFont('dinsmallcapspdf')
+  doc.setTextColor(40)
+  doc.text(str, 320, 9, { align: 'right' })
+}
+
+function addPageHeader(doc, data) {
+  doc.setFont('dinsmallcapspdf')
+  doc.setTextColor(40)
+  if (LOGO_SMALL) {
+    doc.addImage(LOGO_SMALL, 'JPEG', data.settings.margin.left, 5, 18, 5)
+  }
+
+  doc.text('SS2425', data.settings.margin.left + 23, 9)
+  doc.text('100120', data.settings.margin.left + 53, 9)
+  doc.text(
+    'lofoten Goretex pro plus Jacket Ms',
+    data.settings.margin.left + 78,
+    9,
+  )
+  doc.text('031220', data.settings.margin.left + 200, 9)
+  doc.setDrawColor(40)
+  doc.line(data.settings.margin.left, 13, 281, 13)
+}
+
+
 
 function headRows() {
   return [
@@ -219,20 +315,77 @@ function bodyRows(rowCount) {
   for (var j = 1; j <= rowCount; j++) {
     body.push({
       image: '',
-      supplier: 'xxxx',
-      material: 'xxxx',
-      paidby: 'xxxx',
-      size: 's',
-      wheretouse: 'xxx',
-      materialtype: 'xxx',
-      consume: 'xxx',
-      waste: 'xxx',
-      shrink: 'xxx.',
-      csp3cons: 'xxx',
-      patternrotation: 'xxx',
-      garmentrotation: 'xxx',
-      grading: '',
+      supplier: 'Global development',
+      material: 'brSPL70LR/RGD 3L PRO',
+      paidby: 'Norrøna',
+      size: '180 cm',
+      wheretouse: 'Main sewing thread\nFront zipper\nPockets',
+      materialtype: 'Zipper - Ready Length',
+      consume: 'CM',
+      waste: '3%',
+      shrink: '0%',
+      csp3cons: '230',
+      patternrotation: '100',
+      garmentrotation: '100',
+      grading: 'no',
     })
   }
   return body
+}
+
+
+
+function getDataUri (url) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.setAttribute('crossOrigin', 'anonymous'); // getting images from external domain
+
+    image.onload = function () {
+      const canvas = document.createElement('canvas');
+      canvas.width = this.naturalWidth;
+      canvas.height = this.naturalHeight;
+      // next three lines for white background in case png has a transparent background
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = '#fff'; /// set white fill style
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      canvas.getContext('2d').drawImage(this, 0, 0);
+
+      resolve({base64: canvas.toDataURL('image/jpeg'), width:canvas.width, height:canvas.height});
+    };
+
+    image.src = url;
+  });
+}
+
+
+function addFontPack(doc) {
+  doc.addFileToVFS('DINLight-normal.ttf', light)
+  doc.addFont('DINLight-normal.ttf', 'DINLight', 'normal')
+  doc.addFileToVFS('DINNormal-normal.ttf', normal)
+  doc.addFont('DINNormal-normal.ttf', 'DINNormal', 'normal')
+  doc.addFileToVFS('DINBold-normal.ttf', bold)
+  doc.addFont('DINBold-normal.ttf', 'DINBold', 'normal')
+  doc.addFileToVFS('dinsmallcapspdf-normal.ttf', smallcaps)
+  doc.addFont('dinsmallcapspdf-normal.ttf', 'dinsmallcapspdf', 'normal')
+}
+
+function cellBorderLeft(doc, data, color = 200) {
+  doc.setDrawColor(color)
+  doc.line(
+    data.cell.x,
+    data.cell.y,
+    data.cell.x,
+    data.cell.y + data.cell.height,
+  )
+}
+
+function cellBorderTop(doc,data, color=200, padding=0) {
+  doc.setDrawColor(color)
+  doc.line(
+    data.cell.x,
+    data.cell.y,
+    data.cell.x + data.cell.width - padding,
+    data.cell.y,
+  )
 }
